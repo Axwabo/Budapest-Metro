10041392c78a251163452af410799e9a4b251e9c
Axwabo <axwabo@gmail.com>
2026-07-03 22:05:25 +0200

    time scale window

---
+++ Assets/Editor/TimeScaleWindow.cs
@@ -0,0 +1,17 @@
+using UnityEditor;
+using UnityEngine;
+
+public sealed class TimeScaleWindow : EditorWindow
+{
+
+    private void OnGUI() => Time.timeScale = EditorGUILayout.Slider("Time Scale", Time.timeScale, 0, 20);
+
+    [MenuItem("Metro/Time Scale")]
+    private static void ShowWindow()
+    {
+        var window = GetWindow<TimeScaleWindow>();
+        window.titleContent = new GUIContent("Time Scale");
+        window.Show();
+    }
+
+}
