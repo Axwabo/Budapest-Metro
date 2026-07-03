47eb1967b5f393de30fdddcc6b764d26526faa4c
Axwabo <axwabo@gmail.com>
2026-07-03 13:25:00 +0200

    show time

--- Assets/Scripts/Metro/Trains/Routes/OnboardDisplayRenderer.cs
+++ Assets/Scripts/Metro/Trains/Routes/OnboardDisplayRenderer.cs
@@ -24,6 +24,10 @@ namespace Metro.Trains.Routes
 
         private PanelSettings _settings;
 
+        private bool _station;
+
+        private float _time;
+
         public RenderTexture Texture { get; private set; }
 
         private void Awake()
@@ -32,6 +36,15 @@ namespace Metro.Trains.Routes
             _settings = Instantiate(settingsReference);
             _settings.targetTexture = Texture;
             document.panelSettings = _settings;
+        }
+
+        private void Update()
+        {
+            if ((_time -= Time.deltaTime) > 0)
+                return;
+            _station = !_station;
+            _time = 5;
+            _label.text = _station ? JourneyManager.Stop?.Name : Clock.Now.ToString("hh':'mm");
         }
 
         private void OnDestroy()
@@ -43,7 +56,12 @@ namespace Metro.Trains.Routes
 
         protected override void OnInitialized() => _label = document.rootVisualElement.Q<Label>();
 
-        public override void OnStationChanged() => _label.text = JourneyManager.Stop?.Name;
+        public override void OnStationChanged()
+        {
+            _label.text = JourneyManager.Stop?.Name;
+            _time = 5;
+            _station = true;
+        }
 
     }
 
