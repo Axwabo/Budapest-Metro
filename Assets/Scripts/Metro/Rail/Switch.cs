9731cc66bb7ba1487f049a3a9d4e99610f8bebc4
Axwabo <axwabo@gmail.com>
2026-07-05 08:51:30 +0200

    remove InspectorName

--- Assets/Scripts/Metro/Rail/Switch.cs
+++ Assets/Scripts/Metro/Rail/Switch.cs
@@ -10,17 +10,13 @@ namespace Metro.Rail
         private bool isLeft;
 
         [Header("Left State")]
-        [InspectorName("From")]
         public TrackSegment fromLeft;
 
-        [InspectorName("To")]
         public TrackSegment toLeft;
 
         [Header("Right State")]
-        [InspectorName("From")]
         public TrackSegment fromRight;
 
-        [InspectorName("To")]
         public TrackSegment toRight;
 
         public bool IsLeft
