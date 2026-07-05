34c427493f1c047f40d1708723d48d9cc22931f1
Axwabo <axwabo@gmail.com>
2026-07-05 17:44:34 +0200

    begin headlights

---
+++ Assets/Scripts/Metro/Trains/Cars/Headlight.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+namespace Metro.Trains.Cars
+{
+
+    [RequireComponent(typeof(Light))]
+    public sealed class Headlight : CarComponent
+    {
+
+        [SerializeField]
+        private bool isRear;
+
+        [SerializeField]
+        [ColorUsage(false, true)]
+        private Color rearColor;
+
+        [SerializeField]
+        private float rearIntensity;
+
+        private Color _headColor;
+
+        private float _headIntensity;
+
+        private bool? _wasReverse;
+
+        private Light _light;
+
+        private void Awake()
+        {
+            _light = GetComponent<Light>();
+            _headColor = _light.color;
+            _headIntensity = _light.intensity;
+        }
+
+        private void Update()
+        {
+            var reverse = Assembly.Motor.Reverse;
+            if (_wasReverse == reverse)
+                return;
+            _wasReverse = reverse;
+            _light.color = isRear == reverse ? _headColor : rearColor;
+            _light.intensity = isRear == reverse ? _headIntensity : rearIntensity;
+        }
+
+    }
+
+}
