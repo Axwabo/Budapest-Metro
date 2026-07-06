3e61977b226501ca968552584ff8066f0baed440
Axwabo <axwabo@gmail.com>
2026-07-06 11:14:59 +0200

    Clock display

--- Assets/Scripts/Metro/Clock.cs
+++ Assets/Scripts/Metro/Clock.cs
@@ -1,17 +1,43 @@
 using System;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace Metro
 {
 
-    public static class Clock
+    public sealed class Clock : MonoBehaviour
     {
 
-        private static readonly TimeSpan Start = new(14, 19, 42);
+        private static DateTime _start = DateTime.Now;
 
-        public static TimeSpan Now => Start + TimeSpan.FromSeconds(Time.timeSinceLevelLoadAsDouble);
+        [SerializeField]
+        private string start = "14:19:42";
+
+        private UIDocument _document;
+
+        private int _second;
+
+        public static TimeSpan Now => _start.AddSeconds(Time.timeSinceLevelLoadAsDouble).TimeOfDay;
 
         public static float Delta => Time.unscaledDeltaTime * Time.timeScale;
+
+        private void Awake()
+        {
+            if (TimeSpan.TryParse(start, out var startTime))
+                _start = DateTime.Today + startTime;
+            if (!TryGetComponent(out _document))
+                Destroy(this);
+        }
+
+        private void Update()
+        {
+            var now = Now;
+            var second = now.Seconds;
+            if (second == _second)
+                return;
+            _second = second;
+            _document.rootVisualElement.Q<Label>().text = now.ToString("hh':'mm':'ss");
+        }
 
     }
 
