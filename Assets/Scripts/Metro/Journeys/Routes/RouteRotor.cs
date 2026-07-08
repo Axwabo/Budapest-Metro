96f3f58df9e6fb114267b0ebf236d0d43071e2c7
Axwabo <axwabo@gmail.com>
2026-07-08 09:17:00 +0200

    remove check

--- Assets/Scripts/Metro/Journeys/Routes/RouteRotor.cs
+++ Assets/Scripts/Metro/Journeys/Routes/RouteRotor.cs
@@ -77,7 +77,7 @@ namespace Metro.Journeys.Routes
             var now = Clock.Now - StopTimeThreshold;
             foreach (var route in descriptor.GetRoutes())
             {
-                if (_spawnedRoutes.Contains(route) || route.Destination.Time > now)
+                if (_spawnedRoutes.Contains(route))
                     continue;
                 for (var i = 0; i < route.IntermediateStops.Count; i++)
                 {
