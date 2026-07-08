217976f181e534da0b19980dc966605a05fa8134
Axwabo <axwabo@gmail.com>
2026-07-08 08:39:03 +0200

    increase max time scale in editor

--- Assets/Editor/TimeScaleWindow.cs
+++ Assets/Editor/TimeScaleWindow.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public sealed class TimeScaleWindow : EditorWindow
 {
 
-    private void OnGUI() => Time.timeScale = EditorGUILayout.Slider("Time Scale", Time.timeScale, 0, 20);
+    private void OnGUI() => Time.timeScale = EditorGUILayout.Slider("Time Scale", Time.timeScale, 0, 100);
 
     [MenuItem("Metro/Time Scale")]
     private static void ShowWindow()
