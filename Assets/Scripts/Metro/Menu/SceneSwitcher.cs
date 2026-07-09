234226859015e55596ae31f8633fe47d43c5ccca
Axwabo <axwabo@gmail.com>
2026-07-09 12:51:04 +0200

    rename begin + unset start time

--- Assets/Scripts/Metro/Menu/SceneSwitcher.cs
+++ Assets/Scripts/Metro/Menu/SceneSwitcher.cs
@@ -8,14 +8,17 @@ using UnityEditor;
 namespace Metro.Menu
 {
 
-    public sealed class Begin : DocumentComponent
+    public sealed class SceneSwitcher : DocumentComponent
     {
 
         [SerializeField]
         [HideInInspector]
         private string sceneName;
 
-        protected override void Init(VisualElement root) => root.Q<Button>("Play").clicked += () => SceneManager.LoadScene(sceneName);
+        [SerializeField]
+        private string buttonName;
+
+        protected override void Init(VisualElement root) => root.Q<Button>(buttonName).clicked += () => SceneManager.LoadScene(sceneName);
 
 #if UNITY_EDITOR
         [SerializeField]
