5053001e63c1bac6aaa926def171de8a75bad8f9
Axwabo <axwabo@gmail.com>
2026-07-09 15:46:59 +0200

    increase speaker range + begin new delay

--- Assets/Scripts/Metro/Audio/Speaker.cs
+++ Assets/Scripts/Metro/Audio/Speaker.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Metro.Trains;
 using UnityEngine;
 
@@ -8,9 +9,27 @@ namespace Metro.Audio
     public sealed class Speaker : AssemblyComponent
     {
 
+        private readonly List<float[]> _buffers = new();
+
+        private readonly object _lock = new();
+        private int _delaySamples;
+        private int _position;
+
         private AudioSource _source;
 
-        private void Awake() => _source = GetComponent<AudioSource>();
+        private void Start()
+        {
+            _source = GetComponent<AudioSource>();
+            _delaySamples = AudioSettings.outputSampleRate / 2;
+        }
+
+        private void OnAudioFilterRead(float[] data, int channels)
+        {
+            lock (_lock)
+            {
+                // let me think
+            }
+        }
 
         public void PlayOneShit(AudioClip clip, float volumeScale = 1) => _source.PlayOneShot(clip, volumeScale);
 
