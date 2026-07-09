f3b75b7ba32b966af824e944444ff08f677141ea
Axwabo <axwabo@gmail.com>
2026-07-09 19:59:49 +0200

    marl dirty on recalculate + fix left track tunnel overlap

--- Assets/Scripts/Metro/Rail/Controls/ControlPoint.cs
+++ Assets/Scripts/Metro/Rail/Controls/ControlPoint.cs
@@ -1,5 +1,8 @@
 using Metro.Trains.Cars;
 using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 namespace Metro.Rail.Controls
 {
@@ -45,10 +48,16 @@ namespace Metro.Rail.Controls
         [ContextMenu("Recalculate")]
         private void Recalculate()
         {
+            var previousDistance = Distance;
+            var previousReverse = Reverse;
             var t = transform;
             var distance = Track.GetDistance(t.position);
             Distance = Mathf.Approximately(0, distance) ? 0 : Mathf.Clamp(distance, 0, Track.Length);
             Reverse = Vector3.Dot(t.forward, Track.Sample(Distance).forward) < 0;
+            if (Mathf.Approximately(previousDistance, Distance) && previousReverse == Reverse)
+                return;
+            EditorUtility.SetDirty(this);
+            PrefabUtility.RecordPrefabInstancePropertyModifications(this);
         }
 
         private void OnDrawGizmosSelected()
