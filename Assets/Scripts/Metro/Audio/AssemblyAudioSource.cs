7e3d64a45aa7bab5aa4f9a4a812bda00c30c2980
Axwabo <axwabo@gmail.com>
2026-07-09 15:14:17 +0200

    microoptimization
    :3

--- Assets/Scripts/Metro/Audio/AssemblyAudioSource.cs
+++ Assets/Scripts/Metro/Audio/AssemblyAudioSource.cs
@@ -10,9 +10,9 @@ namespace Metro.Audio
     public sealed class AssemblyAudioSource : AssemblyComponent
     {
 
-        private static readonly Comparison<Vector3> DistanceComparison = (a, b) => Vector3.Distance(a, MovementController.LastPosition).CompareTo(Vector3.Distance(b, MovementController.LastPosition));
+        private static readonly Comparison<(Vector3, float)> DistanceComparison = (a, b) => a.Item2.CompareTo(b.Item2);
 
-        private readonly List<Vector3> _closestPoints = new();
+        private readonly List<(Vector3, float)> _closestPoints = new();
 
         private Transform _t;
 
@@ -20,25 +20,30 @@ namespace Metro.Audio
 
         private void LateUpdate()
         {
+            var last = MovementController.LastPosition;
             foreach (var car in Parent.Cars)
             {
                 if (!car.IsPlayerMounted)
                     continue;
-                _t.position = MovementController.LastPosition;
+                _t.position = last;
                 return;
             }
 
             for (var i = 0; i < _closestPoints.Count; i++)
-                _closestPoints[i] = Parent.Cars[i].Bounds.ClosestPoint(MovementController.LastPosition);
+            {
+                var closest = Parent.Cars[i].Bounds.ClosestPoint(last);
+                _closestPoints[i] = (closest, Vector3.Distance(closest, last));
+            }
+
             _closestPoints.Sort(DistanceComparison);
-            _t.position = _closestPoints[0];
+            _t.position = _closestPoints[0].Item1;
         }
 
         protected override void OnInitialized()
         {
             var count = Parent.Cars.Length;
             for (var i = 0; i < count; i++)
-                _closestPoints.Add(Vector3.zero);
+                _closestPoints.Add((Vector3.zero, 0));
         }
 
     }
