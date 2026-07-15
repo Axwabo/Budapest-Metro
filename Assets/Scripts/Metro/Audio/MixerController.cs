0658186d73cf5fea446c06c15bd503f324b6e16a
Axwabo <axwabo@gmail.com>
2026-07-15 08:40:40 +0200

    begin MountState

--- Assets/Scripts/Metro/Audio/MixerController.cs
+++ Assets/Scripts/Metro/Audio/MixerController.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using Metro.Movement;
 using Metro.Trains;
+using Metro.Trains.Doors;
 using Metro.Trains.Driving;
 using UnityEngine;
 using UnityEngine.Audio;
@@ -15,20 +16,42 @@ namespace Metro.Audio
         private AudioMixer mixer;
 
         [SerializeField]
-        private AudioMixerGroup onboard;
+        private AudioMixerGroup outsideClosed;
 
         [SerializeField]
-        private AudioMixerGroup outside;
+        private AudioMixerGroup outsideOpen;
+
+        [SerializeField]
+        private AudioMixerGroup @this;
+
+        [SerializeField]
+        private AudioMixerGroup anotherOpen;
+
+        [SerializeField]
+        private AudioMixerGroup anotherClosed;
 
         private readonly List<IAudioSourceProvider> _providers = new();
 
         private readonly List<AudioSource> _sources = new();
 
+        private DoorController _doors;
+
+        private MountedState _previousState;
+
         private bool? _wasMounted;
+
+        private MountedState MountState => (IsPlayerMounted, Mountable.IsPlayerMounted, _doors.CanDepart) switch
+        {
+            (true, _, _) => MountedState.This,
+            (false, false, true) => MountedState.OutsideClosed,
+            (false, false, false) => MountedState.OutsideOpen,
+            (false, true, true) => MountedState.AnotherClosed,
+            (false, true, false) => MountedState.AnotherOpen
+        };
 
         private void LateUpdate()
         {
-            var mounted = IsPlayerMounted;
+            var state = MountState;
             if (mounted)
                 UpdateMountedVolume();
             if (_wasMounted == mounted)
@@ -45,7 +68,11 @@ namespace Metro.Audio
 
         private void SetVolume(float reductionRatio) => mixer.SetFloat("OutsideVolume", -40 * reductionRatio);
 
-        protected override void OnInitialized() => _providers.AddRange(Parent.Components<IAudioSourceProvider>());
+        protected override void OnInitialized()
+        {
+            _providers.AddRange(Parent.Components<IAudioSourceProvider>());
+            _doors = Parent.RequireComponent<DoorController>();
+        }
 
         private void EnsureCached()
         {
@@ -63,6 +90,17 @@ namespace Metro.Audio
                 foreach (var source in multiple)
                     _sources.Add(source);
             }
+        }
+
+        private enum MountedState
+        {
+
+            OutsideClosed,
+            OutsideOpen,
+            This,
+            AnotherOpen,
+            AnotherClosed
+
         }
 
     }
