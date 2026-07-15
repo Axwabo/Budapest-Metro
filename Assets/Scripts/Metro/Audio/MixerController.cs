efde6bfed39e356acc2b5e1ec1b49e6e2582a822
Axwabo <axwabo@gmail.com>
2026-07-15 08:20:42 +0200

    what am i doing

--- Assets/Scripts/Metro/Audio/MixerController.cs
+++ Assets/Scripts/Metro/Audio/MixerController.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using Metro.Movement;
 using Metro.Trains;
 using Metro.Trains.Driving;
@@ -14,16 +13,6 @@ namespace Metro.Audio
         [SerializeField]
         private AudioMixer mixer;
 
-        [SerializeField]
-        private AudioMixerGroup onboard;
-
-        [SerializeField]
-        private AudioMixerGroup outside;
-
-        private readonly List<IAudioSourceProvider> _providers = new();
-
-        private readonly List<AudioSource> _sources = new();
-
         private bool? _wasMounted;
 
         private void LateUpdate()
@@ -33,37 +22,19 @@ namespace Metro.Audio
                 UpdateMountedVolume();
             if (_wasMounted == mounted)
                 return;
-            EnsureCached();
-            var group = mounted ? onboard : outside;
-            foreach (var provider in _sources)
-                provider.outputAudioMixerGroup = group;
-            if (!mounted && !Mountable.IsPlayerMounted)
-                SetVolume(0);
+            if (mounted || Mountable.IsPlayerMounted)
+                return;
+            SetVolume(0);
+            mixer.SetFloat("OnboardVolume", -40);
         }
 
-        private void UpdateMountedVolume() => SetVolume(State == DriverState.Driving ? 1 : 0);
+        private void UpdateMountedVolume()
+        {
+            SetVolume(State == DriverState.Driving ? 1 : 0);
+            mixer.SetFloat("OnboardVolume", 0);
+        }
 
         private void SetVolume(float reductionRatio) => mixer.SetFloat("OutsideVolume", -40 * reductionRatio);
-
-        protected override void OnInitialized() => _providers.AddRange(Parent.Components<IAudioSourceProvider>());
-
-        private void EnsureCached()
-        {
-            if (_sources.Count != 0)
-                return;
-            foreach (var provider in _providers)
-            {
-                var multiple = provider.MultipleAudioSources;
-                if (multiple.Length == 0)
-                {
-                    _sources.Add(provider.SingleAudioSource);
-                    continue;
-                }
-
-                foreach (var source in multiple)
-                    _sources.Add(source);
-            }
-        }
 
     }
 
