bbfa3560b74c4bd32d766449f328c0951e4d66de
Axwabo <axwabo@gmail.com>
2026-09-01 17:13:06 +0200

    Revert "mcos"
    This reverts commit 29e23cd93c76eaa27f444e235b98e4d0e7927a5e.

--- Assets/Editor/Build.cs
+++ Assets/Editor/Build.cs
@@ -13,8 +13,6 @@ public static class Build
 
     public static void Linux() => Run(BuildTarget.StandaloneLinux64, "Budapest-Metro.x86_64");
 
-    public static void MacOS() => Run(BuildTarget.StandaloneOSX, "Budapest-Metro");
-
     public static void WebGL() => Run(BuildTarget.WebGL, "Budapest-Metro"); // TODO: idk the extension lmao
 
     private static void Run(BuildTarget target, string filename)
