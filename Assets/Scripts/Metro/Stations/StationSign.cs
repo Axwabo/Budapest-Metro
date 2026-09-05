734cf3292952e14fb6b0a6ddad0bc9a4ba67cf4a
Axwabo <axwabo@gmail.com>
2026-09-05 07:05:38 +0200

    separate M4 styles + M3 sign progres + don't destroy document

--- Assets/Scripts/Metro/Stations/StationSign.cs
+++ Assets/Scripts/Metro/Stations/StationSign.cs
@@ -100,8 +100,8 @@ namespace Metro.Stations
 
         private void Dispose()
         {
-            Destroy(document);
-            enabled = false;
+            // Destroy(document);
+            // enabled = false;
             _station.RenderQueuedSigns.Remove(this);
         }
 
